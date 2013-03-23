e786e5a93aea63cfe892c051d80b1be4bcae0c4b
koval <koval@ubuntu001>
2013-03-23 17:14:54 +0900

    distribution graph you cannot imagine.

--- experimental/clock/clocktest.hpp
+++ experimental/clock/clocktest.hpp
@@ -14,11 +14,74 @@
 #include "cpufreq.hpp"
 #include <functional>
 #include <vector>
+#include "plplot/plplot.h"
+#include "plplot/plstream.h"
+#include <cmath>
+
+struct statistics{
+	statistics(std::vector<uint64_t>& samples)
+	: nDivision(50)
+	{
+		xmin = samples[0];
+		xmax = samples[0];
+		mean = variance = quadraticsum = stdev = 0;
+		for (uint32_t i=0;i<samples.size();++i){
+			mean += samples[i];
+			quadraticsum += (double)((double)samples[i] * samples[i]);
+			xmin = std::min(xmin, (double)samples[i]);
+			xmax = std::max(xmax, (double)samples[i]);
+		}
+		mean /= samples.size();
+		variance = quadraticsum/samples.size() - mean * mean;
+		variance *= samples.size() / (samples.size() - 1.0);
+		stdev = std::sqrt(variance);
+		populations.clear();
+		populations.resize(nDivision+1);
+		xmedian.resize(nDivision+1);
+		for (uint32_t i=0;i<samples.size();++i){
+			double lgap = samples[i] - xmin;
+			double rgap = xmax - samples[i];
+			double part = (lgap) / (lgap + rgap);
+			part *= nDivision;
+			++populations[(int)part];
+		}
+		ymin = populations[0]; ymax = populations[0];
+		for (int i=0;i<=nDivision;++i){
+			double xVal = (xmin * i + xmax * (nDivision - i)) / nDivision;
+			xmedian[i] = xVal;
+			ymin = std::min(ymin, (double)populations[i]);
+			ymax = std::max(ymax, (double)populations[i]);
+		}
+	}
+
+	void plot(){
+		plstream p;
+		p.init();
+		p.env(xmin,xmax,ymin,ymax,0,0);
+		p.lab("(x)","(y)", "distribution");
+		for (int i=0;i<(int)xmedian.size()-1;++i){
+			p.join(xmedian[i],populations[i],xmedian[i+1],populations[i+1]);
+		}
+
+	}
+	double mean;
+	double quadraticsum;
+	double variance;
+	double stdev;
+	double xmin;
+	double xmax;
+	double ymin;
+	double ymax;
+	int nDivision;
+	std::vector<int> populations;
+	std::vector<double> xmedian;
+};
 
 struct timer{
 	timer(std::function<uint64_t()> func, uint64_t unit, uint64_t mult) : func(func), unit(unit), mult(mult)
 	{
 		initTime = func();
+		times.resize(1000000);
 	}
 	void init(){
 		initTime = func();
@@ -64,7 +127,7 @@ extern inline timespec gettimefunc(){
 extern inline uint64_t gettime64(){
 	timespec ts;
 	clock_gettime(CLOCK_REALTIME, &ts);
-	return (ts.tv_sec * 1000000000) + ts.tv_nsec;
+	return ((uint64_t)ts.tv_sec * 1000000000) + ts.tv_nsec;
 }
 
 
@@ -76,6 +139,8 @@ inline void clocktest(){
 	};
 	timer rdtsctimer(rdtscfnc, cpufreq(), 1000000000);
 	timer tftimer(gtfnc, 1, 1);
+
+	/*
 	for (int i=0;i<1000;++i){
 		rdtsctimer.settime();
 		tftimer.settime();
@@ -86,23 +151,12 @@ inline void clocktest(){
 	for (int i=0;i<1000;++i){
 		printf("rdtsc %lld ns\n",rdtsctimer.times[i]);
 		printf("gett  %lld ns\n",tftimer.times[i]);
-	}
+	}*/
 
 	int nSpeedTest = 100000;
-	/*
-	printf("timer speed test\n");
-	rdtsctimer.clear();
-	for (int i=0;i<nSpeedTest;++i){
-		rdtsctimer.settime();
-	}
-	printf("%d times rdtsc : %lld ns\n", nSpeedTest, rdtsctimer.times.back());
-	tftimer.clear();
-	for (int i=0;i<nSpeedTest;++i)
-		tftimer.settime();
-	printf("%d times gett  : %lld ns\n", nSpeedTest, tftimer.times.back());
-	*/
 
-	printf("timer speed test\n");
+
+	printf("timer speed test(function call only)\n");
 	rdtsctimer.clear();
 	for (int i=0;i<nSpeedTest;++i){
 		rdtsc64();
@@ -110,7 +164,7 @@ inline void clocktest(){
 	rdtsctimer.settime();
 	printf("%d times rdtsc : %lld ns\n", nSpeedTest, rdtsctimer.times.back());
 
-	printf("timer speed test\n");
+	//printf("timer speed test\n");
 	rdtsctimer.clear();
 	timespec ts;
 	for (int i=0;i<nSpeedTest;++i){
@@ -119,7 +173,28 @@ inline void clocktest(){
 	}
 	rdtsctimer.settime();
 	printf("%d times gett  : %lld ns\n", nSpeedTest, rdtsctimer.times.back());
+
+
+	printf("timer speed test(entire timer call)\n");
+	rdtsctimer.clear();
+	for (int i=0;i<nSpeedTest;++i){
+		rdtsctimer.settime();
+	}
+	printf("%d times rdtsc : %lld ns\n", nSpeedTest, rdtsctimer.times.back());
+	statistics rdtstat(rdtsctimer.times);
+	printf("mean : %lf stdev : %lf ns\n", rdtstat.mean, rdtstat.stdev);
+
+	tftimer.clear();
+	for (int i=0;i<nSpeedTest;++i)
+		tftimer.settime();
+	printf("%d times gett  : %lld ns\n", nSpeedTest, tftimer.times.back());
+	statistics gettstat(tftimer.times);
+	printf("mean : %lf stdev : %lf ns\n", gettstat.mean, gettstat.stdev);
+
+	rdtstat.plot();
+	//gettstat.plot();
 }
+
 
 
 
