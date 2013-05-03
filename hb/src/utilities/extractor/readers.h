bec6815d1055b75d4a7d418e21b4b4672c2c1971
koval12 <koval12@naver.com>
2013-05-03 17:52:40 +0900

    Create readers.h

---
+++ hb/src/utilities/extractor/readers.h
@@ -0,0 +1,40 @@
+#include "krx_kospi_futures.hpp"
+#include "krx_kospi_options.hpp"
+#include "rawdatareader.hpp"
+
+class DataReader{
+public:
+  DataReader(const std::string& filename) : rd(filename){}
+	virtual void setType() = 0;
+	bool next(){
+		bool ret = rd.next();
+		len = rd.sz;
+		content = rd.msgbuf;
+		setType();
+		return ret;
+	}
+	bool empty(){
+		return rd.empty();
+	}
+	RawDataReader rd;
+	char* content;
+	int len;
+};
+
+class KospiOptionsReader : public DataReader{
+public:
+	KospiOptionsReader(const std::string& filename) : DataReader(filename) {}
+	void setType(){
+		optionsType = getTypeKrxOptionsHeader(content);
+	}
+	eKrxOptionsHeader optionsType;
+};
+
+class KospiFuturesReader : public DataReader{
+public:
+	KospiFuturesReader(const std::string& filename) : DataReader(filename) {}
+	void setType(){
+		futuresType = getTypeKrxFuturesHeader(content);
+	}
+	eKrxFuturesHeader futuresType;
+};
