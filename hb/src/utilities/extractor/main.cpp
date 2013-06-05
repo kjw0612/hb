6f07be23da87f88939ca75f7e47256e7e58d3f95
koval12 <koval12@naver.com>
2013-06-05 18:23:40 +0900

    idea has settled.
    the remaining work should be done ASAP.

--- hb/src/utilities/extractor/main.cpp
+++ hb/src/utilities/extractor/main.cpp
@@ -8,6 +8,7 @@
 #include "indexer.h"
 #include "plotter.h"
 #include "readerstatic.h"
+#include "analysis_trajectory.h"
 
 #ifdef BOOST
 #include <boost/timer.hpp>
@@ -15,62 +16,6 @@
 
 typedef std::vector<int> v_i;
 typedef std::vector<double> v_d;
-
-std::pair<v_i, v_d> greek_trajectory
-	(double fut_price0, const std::pair<v_i, v_d>& opt_price, const std::pair<v_i, v_d>& opt_greek)
-{
-	std::pair<v_i, v_d> ret;
-	if (opt_price.first.size() == opt_price.second.size() && 
-		opt_greek.first.size() == opt_greek.second.size() &&
-		opt_price.first.size() == opt_greek.first.size())
-	{
-		ret.first = opt_price.first;
-		ret.second = v_d(opt_price.second.size());
-		double opt_price0 = opt_price.second[0];
-		for (int i=0;i<(int)opt_greek.second.size();++i){
-			double opt_pricei = opt_price.second[i];
-			double opt_deltai = opt_greek.second[i];
-			double opt_price_diff = opt_pricei - opt_price0;
-			double fut_price_diff = opt_price_diff / opt_deltai;
-			ret.second[i] = fut_price_diff + fut_price0;
-		}
-		return ret;
-	}
-	else{
-		return ret;
-	}
-}
-
-std::pair<std::pair<v_i, v_d>, std::pair<v_i, v_d> > get_opt_greek_series
-(const std::string& krcode, char callput, int start_time, int end_time, Brick::DataType dataType = Brick::MidPrice)
-{
-	const std::vector<Brick *>& greeksbricks = ReaderStatic::get().greeksbase().get(krcode);
-	const std::vector<Brick *>& optbricks =
-		(callput == 'c') ? ReaderStatic::get().callbase().get(krcode) : ReaderStatic::get().putbase().get(krcode);
-
-	std::pair<v_i, v_d> optseries = bricks2MidPriceGrid(optbricks, start_time, end_time, 5000, dataType);
-	std::pair<v_i, v_d> greeksseries = bricks2MidPriceGrid(greeksbricks, start_time, end_time, 5000, Brick::Delta);
-	return std::make_pair(optseries,greeksseries);
-}
-
-std::pair<v_i, v_d> get_trajectory
-(double fut_price0, const std::string& krcode, char callput, int start_time, int end_time, Brick::DataType dataType = Brick::MidPrice)
-{
-	std::pair<std::pair<v_i, v_d>, std::pair<v_i, v_d> > ogseries = get_opt_greek_series(krcode, callput, start_time, end_time, dataType);
-	return greek_trajectory(fut_price0, ogseries.first, ogseries.second);
-}
-
-std::pair<v_i, v_d> get_synthetic_trajectory(double fut_price0, const std::string& krcode, char callput, 
-											 const std::string& krcode_2, char callput_2, int start_time, int end_time,
-											 Brick::DataType dataType = Brick::MidPrice, Brick::DataType dataType2 = Brick::MidPrice)
-{
-	std::pair<std::pair<v_i, v_d>, std::pair<v_i, v_d> > ogseries1 = get_opt_greek_series(krcode, callput, start_time, end_time, dataType);
-	std::pair<std::pair<v_i, v_d>, std::pair<v_i, v_d> > ogseries2 = get_opt_greek_series(krcode_2, callput_2, start_time, end_time, dataType2);
-	std::pair<v_i, v_d> syn_opt_series = aXpbY(1, ogseries1.first, -1, ogseries2.first);
-	std::pair<v_i, v_d> syn_greeks_series = aXpbY(1, ogseries1.second, -1, ogseries2.second);
-	return greek_trajectory(fut_price0, syn_opt_series, syn_greeks_series);
-}
-
 
 void setup_time(int start_time, int end_time)
 {
